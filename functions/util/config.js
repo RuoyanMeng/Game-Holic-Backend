42940ee17d074f700c1fbccb23c1022d1cc3e60c
RuoyanMeng <ruoyanmeng@gmail.com>
2019-11-23 20:08:13 +0200

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports  = {
-    apiKey: "AIzaSyBVeT1XcYFz4miqIoYbjNqE_sH46hIgYNQ",
-    authDomain: "game-holic-590ee.firebaseapp.com",
-    databaseURL: "https://game-holic-590ee.firebaseio.com",
-    projectId: "game-holic-590ee",
-    storageBucket: "game-holic-590ee.appspot.com",
-    messagingSenderId: "545901800708",
-    appId: "1:545901800708:web:b37410b826b77cb2d47451"
-  };
